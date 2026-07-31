e92d2217475d9f62bb5fd01619d2f125f9a6ec76
Moustapha BALDE <moustaphabaldehotmail@gmail.com>
2026-07-31 18:32:29 +0000

    modifier

--- front/src/app/vendeur/commandes/commandes.ts
+++ front/src/app/vendeur/commandes/commandes.ts
@@ -1,9 +1,61 @@
 import { Component } from '@angular/core';
+import { CommandeHeader } from './components/commande-header/commande-header';
+import { CommandeFilter } from './components/commande-filter/commande-filter';
+import { CommandeTable } from './components/commande-table/commande-table';
 
 @Component({
   selector: 'app-commandes',
-  imports: [],
+  imports: [CommandeHeader, CommandeFilter, CommandeTable],
   templateUrl: './commandes.html',
   styleUrl: './commandes.css',
 })
-export class Commandes {}
+export class Commandes {
+  chargerCommandes(): void {
+    console.log('Actualisation des commandes');
+  }
+  appliquerFiltres(filtres:any){
+
+  console.log(filtres);
+
+}
+commandes: any[] = [];
+
+ouvrirApercu(commande: any) {
+
+  console.log(commande);
+
+}
+
+
+
+ouvrirFacture(commande: any) {
+
+  console.log(commande);
+
+}
+commandeSelectionnee: any = null;
+
+afficherStatut = false;
+changerStatut(commande: any) {
+
+  this.commandeSelectionnee = commande;
+  console.log(commande);
+
+  this.afficherStatut = true;
+
+}
+
+fermerStatut() {
+
+  this.afficherStatut = false;
+
+}
+
+enregistrerStatut(statut: string) {
+
+  console.log(statut);
+
+  this.afficherStatut = false;
+
+}
+}
