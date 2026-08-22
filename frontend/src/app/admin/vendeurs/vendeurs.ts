2a34af851922eae291b714cd2c1d7b477a8760ac
Moustapha BALDE <moustaphabaldehotmail@gmail.com>
2026-08-22 17:57:28 +0000

    Supprimer les projets frontend dupliques

--- frontend/src/app/admin/vendeurs/vendeurs.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-vendeurs',
-  imports: [],
-  templateUrl: './vendeurs.html',
-  styleUrl: './vendeurs.css',
-})
-export class Vendeurs {}
